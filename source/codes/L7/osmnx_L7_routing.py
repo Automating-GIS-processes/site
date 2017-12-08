0e25bc37f845439c4c0d5be9acf7ddc4379a549c
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2017-12-08 12:00:59 +0200

    Added L6 lecture videos and osmnx code templates

---
+++ source/codes/L7/osmnx_L7_routing.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Osmnx routing by driving.
+
+Created on Thu Dec  7 15:49:53 2017
+
+@author: Henrikki Tenkanen
+"""
+
+# Simple network routing with osmnx
+# =================================
+
