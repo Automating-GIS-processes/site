9b57d6d3cb0c59d574f5748a105c086b2c0c378e
htenkanen <henrikki.tenkanen@helsinki.fi>
2018-10-29 20:35:27 +0200

    Minor fix to conf

--- source/conf.py
+++ source/conf.py
@@ -184,7 +184,7 @@ html_context = {
     # Set the following variables to generate the resulting github URL for each page.
     # Format Template: https://{{ github_host|default("github.com") }}/{{ github_user }}/{{ github_repo }}/blob/{{ github_version }}{{ conf_py_path }}{{ pagename }}{{ suffix }}
     'github_user': 'Automating-GIS-processes',
-    'github_repo': '2018',
+    'github_repo': '2018/',
     'github_version': 'master/source/'
 }
 
