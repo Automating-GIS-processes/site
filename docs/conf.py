2b45afcbb7fcca26ed2dab15c283e5dc49fdcc19
Christoph Fink <christoph.fink@gmail.com>
2022-10-15 00:49:06 +0300

    first bits of lesson 1, notebook generation works, also binder

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ myst_substitutions = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_logo = "_static/img/autogis-logo_300x210px.svg"
+html_logo = "_static/images/autogis-logo_300x210px.svg"
 html_short_title = "AutoGIS"
 html_title = ""
 
